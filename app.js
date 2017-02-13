b78f54fa2a0050635c23861145af76f90a1fe7fc
James Branca <paceyachter@gmail.com>
2017-02-13 10:14:12 -0800

    update

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ if(quizQuestion) {
   console.log('ok! lets play');
 } else{
   console.log('are you sure you dont want to play?');
+  alert('are you sure you dont want to play');
 }
 //starting to formulate questions in a Yes or No format--#1 out of 5 questions.
 function questionOne(){
   quizQuestion = prompt('please answer with Y or N. Does James enjoy fishing?');
   if(quizQuestion.toUpperCase() === 'Y') {
     console.log('yes, he loves to catch Alaskan Salmon!');
+    alert('yes, he loves to catch fresh Alaskan Salmon!');
   } else if (quizQuestion.toUpperCase() === 'N') {
     console.log('No! Great White sharks like to bite him in the ass');
+    alert('No way! fising is to boring');
   }
 }
 // question 2 for project
@@ -21,8 +24,10 @@ function questionTwo(){
   quizQuestion = prompt('please answer with Y or N. Does James watch Game of Thrones?');
   if(quizQuestion.toUpperCase() === 'Y') {
     console.log('yes, he loves to watch Jon Snow die!');
+    alert('yes, he loves to watch Jon Snow die!');
   } else if (quizQuestion.toUpperCase() === 'N') {
     console.log('No, Cersei Lannister is a raging bitch!');
+    alert('No, Cersei Lannister is a raging bitch');
   }
 }
  //question 3 for the project
@@ -32,15 +37,17 @@ function questionThree(){
     console.log('yes, he loves to fly to Paris and Zurich!');
   } else if (quizQuestion.toUpperCase() === 'N') {
     console.log('No, he flunked out of the Naval Aviator program!');
+    alert('No, he flunked out of the Naval Aviator progam');
   }
 }
   //question 4 in my quiz series for the project
 function questionFour(){
-  quizQuestion = prompt('please answer with Y or N. Does James scuba dive off of his yacht in the Florida ?');
+  quizQuestion = prompt('please answer with Y or N. Does James scuba dive off of his boat?');
   if(quizQuestion.toUpperCase() === 'Y') {
     console.log('yes, he loves to dive and sail!');
   } else if (quizQuestion.toUpperCase() === 'N') {
-    console.log('No, his yacht sank and he floated until the coast guard rescued him!');}
+    console.log('No, gas is way too much money!');
+    alert('Nope. Gas is way too much money!'); }
 }
 //fifth and final project question
 function questionFive(){
@@ -64,9 +71,31 @@ function questionSix(){
     }
   }
 }
-questionOne();
-questionTwo();
-questionThree();
-questionFour();
-questionFive();
-questionSix();
+
+// Question 7: Here we are required to have another guessing questions. This question has 3 requirements.
+// 1. Answer's must be stored in an array. 2. The user only has 6 tries. 3. The user gets a message if they guess correct of if they run out of tries.
+function questionSeven(){
+var vacationSpots = ['Paris', 'New York', 'Barcelona','England'];
+
+  for (var i = 0; i < vacationSpots.length; i++){
+ console.log(vacationSpots[i]);
+}
+  var howManyCountries = 4;
+  for(var i = 0; i < 1; i++){
+    var quizQuestion = prompt('What countries did James visit ?');
+    if(quizQuestion == vacationSpots.length) {
+      alert('too high');
+    } else if(quizQuestion < howManyFish){
+      alert(' too low');
+    }else{
+      alert('Great Job!');
+    }
+  }
+}
+  questionOne();
+  questionTwo();
+  questionThree();
+  questionFour();
+  questionFive();
+  questionSix();
+  questionSeven();
